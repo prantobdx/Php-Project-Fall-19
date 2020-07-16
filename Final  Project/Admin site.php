ab35a3c96b417e93005daf4ad03070c07ad397c0
prantobdx <33161003+prantobdx@users.noreply.github.com>
2020-07-16 23:17:55 +0600

    Add files via upload

---
+++ Final  Project/Admin site.php
@@ -0,0 +1,163 @@
+
+<html>
+<head>
+<link href='Responsive Layout--1.css' rel='stylesheet'>
+<title> Admin </title>
+</head>
+
+
+
+<body bgcolor="DarkCyan">
+
+<h1 align="center" style="color:DarkGreen" >Control your Bussiness </h1>
+
+
+<div align = "center"> <img src = "Image and videos\Bok cover -1.jpg" 
+ alt="image of a t-shirt" hspace="10"
+ height="350" width="280"
+ border="5" style="color:Orange"/>
+
+
+
+<h3 style="color:fuchsia" ><b>Upload Product Picture </b></h3>
+<enctype="multipart/formdata"><input type="file" name="user_file"/>
+
+<br/><br/>
+
+<div align="center"><input type="submit" value="save" style="color:green;"/></div>
+
+<br/>
+<font align="center" style="color:tomato" size="4" face="arial black"/> set &nbsp; product amount
+<p align="center"> <input type="int"  size="25" placeholder="Set-amount"/>
+
+<p> <font size="4" align="center" style="color:Navy";/> enter contact number 
+<p align="center"> <input type=""  size="25" placeholder="your-mobile- number"/></p>
+
+<hr><p align ="center"><input type="submit" value = "ok" style="color:red;"></hr>
+
+<div>
+<h3><font Style="color:black" align="center" face="Arial"/> 
+
+<p Style="background-color:yellow"> When upload your product pictures <br/>You have must to 
+write about your product in short discription.</p>
+
+
+<div>
+
+<form ="Conn.php" method="Post">
+<ul class=new>
+
+<li style="color:Navy">  product_type&nbsp;&nbsp <input type="text" name="Product_type"/></li>
+<li style="color:red">   product_color    <input type="text" name="product_color"/></li>
+<li style="color:maroon">shop-name &nbsp;&nbsp;&nbsp; <input type="text" name="Shop_Name"></li>
+<li style="color:purple">shop Adress&nbsp;&nbsp; <input type="text" name="shop_Adress"/></li>
+</ul>
+<label> <button type="Submit"Style="background-color:yellow" name="done"> Save </button> </label>
+</form>
+</div>
+ 
+ 
+ <br/>
+ <br/>
+<a href="http://www.google.com/" target="_blank"><p align="left"> google Home</p></a>
+
+<a href ="https://www.youtube.com/watch?v=JdZTlGK--QQ" target="_blank"> <p align="right">YOUR PRODUCT VIDEOS </p>
+
+<b><a href = "mailto:pranto35-1858@diu.edu.bd?subject=You can do &body=this is your future"> 
+<p align="left">Set your mail Adress</p></a></b>
+
+<a href= "mailto: a@b.com?subject=Mail for product order & body=I like your product id=........   
+my contact number=......." >
+<p align="left"> witting Mail Example</p></a>
+
+
+
+<h3 align="center" style="color:olive"><a href="#top">UP TO PAGE</a></h3>
+
+<iframe width="100%" name="shopping" src="https://www.daraz.com.bd/mens-t-shirts/" 
+seamless="seamless">update your borwser
+</iframe>
+<br/>
+<br/>
+
+
+<b><div style="color:black"> {if product amount &nbsp; &gt;1000}</div></b>
+
+<!--Table create-->
+
+
+<!-- <tr>
+<td> T-shirt</td>
+<td>Green</td>
+<td>400</td>
+<td>01749799858</td>
+<td>Dhaka Shop</td>
+<td>Dhaka</td>
+<td>Dhaka-1217</td>
+</tr>
+
+<tr>
+<td> polo-shirt </td>
+<td> Blue </td>
+<td> 550 </td>
+<td>01701369867</td>
+<td>Dhaka Shop</td>
+<td>Dhaka</td>
+<td>Dhaka-1207</td>
+</tr>
+
+</table></div> !-->
+
+<br/>
+
+
+
+
+<p align="center"><strong>Note:</strong><a style="color:bule"> This is one kind sample of our product details</p>
+
+<h1 style="color:tomato">This is your own site</h1>
+
+
+<div style="color:white" float:left;">
+<h2></b>Just Try yourself!</b></h2>
+</div>
+
+<div> <p align="left" style="color:blue"><b><i>Search</i> <br/>
+<a href="file:///C:/Users/daspr/Desktop/HTML/First%20Webpage%20create%20.html"
+target="blank"><i style="color:maroon"><code>website link</code>
+</i></a></p></div>
+
+<pre align="right" style="color:red">
+<b>[Show all size and color 
+   that are available 
+       about your product. 
+	       Thank you!]</b>
+</pre>
+
+<h3 style="color:maroon">Here you have to submit <sup>All-best</sup >product Only.
+{At least 2 <sup>2</sup> =4<sub>sizes</sub>} </h3>
+
+<h4 style="color:blue"> <b><i>Not supprted only</i> <del>one color or size </del> </b></h4>
+
+<form action="processor.php" method="get">
+<b>Size<input type="number" name="size" maxlength="3" placeholder="pices of size"/>
+<b>Color<input type="number" name="color" placeholder="Number_Of_All-color" maxlength="10"/>
+<input type="submit" name="submit" value="submit"/>
+</form>
+
+<br/>
+
+
+<br/>
+<br/>
+<div  id="1st" align="right">
+<button > <a href="User site.html"  target="blank" >Go to Shop </a> </button>
+<div>
+<br/>
+<table align="center" bgcolor="color:maroon" style="color:yellow" border="10" cellspacing="15"
+cellpadding="20">
+
+
+</body>
+</html>
+
